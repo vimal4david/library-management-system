f1094fdb0583e4a3b84fa7c36261b220b4608134
Vimal David Vincent Devaraj <VimalDavid.VincentDevaraj@costain.com>
2023-09-25 07:31:24 +0100

    Completed implementation for LoanServiceImpl

--- src/main/java/com/vdavid/apps/librarymanagementsystem/model/Item.java
+++ src/main/java/com/vdavid/apps/librarymanagementsystem/model/Item.java
@@ -1,4 +1,4 @@
 package com.vdavid.apps.librarymanagementsystem.model;
 
-public record Item(Integer uniqueId, Integer ItemId, ItemType itemType, String title) {
+public record Item(Long uniqueId, Integer ItemId, ItemType itemType, String title) {
 }
